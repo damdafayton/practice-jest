d4d87ca7adc7acf92c713e25b65c5b283f28e1c7
backtofayton <yucelolcay@gmail.com>
2022-02-19 09:15:39 +0200

    Update

--- tasks.test.js
+++ tasks.test.js
@@ -3,6 +3,8 @@ import { reverseString } from "./task2"
 import { Calculator } from "./task3"
 import { capitalizeString } from "./task4"
 
+
+// Task 1
 test('book has 4 letters', () => {
     expect(stringLength('book')).toBe(4)
 })
@@ -11,10 +13,12 @@ test('string must have at least 1 and at most 10 characters', () => {
     expect(() => stringLength()).toThrow('String length must be between 1 and 10 characters')
 })
 
+// Task 2
 test('"book" must return reverse of it which is "koob"', () => {
     expect(reverseString('book')).toBe('koob')
 })
 
+// Task 3
 const numberSetToBeTested = [
     { a: 2, b: 3, add: 5, substract: -1, divide: 2 / 3, multiply: 6 },
     { a: 6, b: 4, add: 10, substract: 2, divide: 6 / 4, multiply: 24 },
@@ -45,6 +49,7 @@ describe.each(numberSetToBeTested)('multiply method for $a and $b', ({ a, b, mul
     })
 })
 
+// Task 4
 describe('capitalize string', () => {
     test('is a string', () => {
         expect(() => capitalizeString(123)).toThrow('Please input a string')
